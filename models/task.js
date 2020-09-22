48e83c58c0704ab059efce99d33ea6dc069bbaae
Mohan <mohanu526@gmail.com>
2020-09-23 01:33:51 +0530

    added tasks-list Data Model

---
+++ models/task.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+const tasksSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    status: {type: Number},
+    priority: {type: Number, required: true},
+    assignedBy: {type: mongoose.Types.ObjectId, ref: 'Admins'},
+    givenDate:  {type: Date, required: true},
+    deadline:  {type: Date, required: true}
+});
+
+tasksSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('tasks', tasksSchema);
