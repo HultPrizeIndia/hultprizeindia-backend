a519e0422f4661a45d7654ad4365828082aaf243
Mohan <mohanu526@gmail.com>
2020-09-23 01:20:08 +0530

    updated Admin Data Model

--- models/admin.js
+++ models/admin.js
@@ -3,12 +3,14 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 
 const adminSchema = new mongoose.Schema({
-    username: {type: String, required: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    image: {type: String},
+    // image: {type: String},
     password: {type: String, required: true, minlength: 6},
     mobile: {type: String, required: true},
-    campusDirectors: [{type: mongoose.Types.ObjectId, ref: 'CampusDirector'}]
+    joinDate:{type: Date, required: true}
+    // campusDirectors: [{type: mongoose.Types.ObjectId, ref: 'CampusDirector'}]
 });
 
 adminSchema.plugin(uniqueValidator);
