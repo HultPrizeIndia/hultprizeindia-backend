7c9247e4bb4a377ab93e82a8f745d4b93b249d0c
sarcastic-verma <shivamthegreat.sv@gmail.com>
2020-09-24 01:48:21 +0530

    - blacklist model added

---
+++ models/blacklist.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const blacklistSchema = new mongoose.Schema({
+    tokens: {type:String,required:true},
+    listedOn: {type:Date,required:true}
+});
+
+module.exports = mongoose.model('Blacklist', blacklistSchema);
