af8c6c7160af7aa34ba495fa56f87722f797bd0a
dramikei <raghav2011rv@gmail.com>
2020-09-24 17:55:24 +0530

    a bit better error message

--- index.js
+++ index.js
@@ -35,18 +35,6 @@ app.use((req, res, next) => {
 
 // Handle File upload:
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
-app.use((error, req, res, next) => {
-    if (req.file) {
-        fs.unlink(req.file.path, err => {
-            console.log(err);
-        });
-    }
-    if (res.headersSent) {
-        return next(error);
-    }
-    res.status(error.code || 500);
-    res.json({message: error.message || 'An unknown error occurred!'});
-});
 
 
 // Setup Routes:
@@ -62,6 +50,22 @@ app.use((req, res, next) => {
     throw new RequestError('Could not find this route.', 404);
 });
 
+// Error Handling
+app.use((error, req, res, next) => {
+    if (req.file) {
+        fs.unlink(req.file.path, err => {
+            console.log(err);
+        });
+    }
+    if (res.headersSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({
+        "status": "failed",
+        "message": error.message || 'An unknown error occurred!'
+    });
+});
 
 mongoose.connect(`${process.env.DB_URL}`, {
     useNewUrlParser: true, 
