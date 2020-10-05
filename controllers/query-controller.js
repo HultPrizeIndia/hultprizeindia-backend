57ed357f5dc332ffa6348cb721231038357fe941
dramikei <raghav2011rv@gmail.com>
2020-10-06 02:06:20 +0530

    completed query controller

--- controllers/query-controller.js
+++ controllers/query-controller.js
@@ -26,9 +26,107 @@ const getAllQuery = async (req, res, next) => {
         const error = new RequestError('Fetching queries failed, please try again later.', 500, err);
         return next(error);
     }
-    await res.json({"status": "success", referrals: referrals.map(referral => referral.toObject({getters: true}))});
+    await res.json({"status": "success", "queries": queries.map(query => query.toObject({getters: true}))});
+}
+
+const createQuery = async (req, res, next) => {
+    const errors = validationResult(req);
+    const raisedBy = req.userData.userId;
+
+    if (!errors.isEmpty()) {
+        return next(
+            new RequestError('Invalid Inputs passed', 422, errors)
+        );
+    }
+
+    const {title, description, comment, raisedFor} = req.body;
+    const raiseDate = Date().toLocaleString();
+    const query = new Query({
+        title,
+        description,
+        comment,
+        raisedBy,
+        raisedFor,
+        raiseDate
+    });
+    try {
+        await query.save();
+    } catch (err) {
+        const error = new RequestError("Error creating query", 500, err);
+        return next(error);
+    }
+    res.json({"status": "success", "query":query});
+
+}
+
+const updateQuery = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return next(
+            new RequestError('Invalid Inputs passed', 422, errors)
+        );
+    }
+    const queryId = req.params.queryId;
+    const { title, description, comment, raisedFor } = req.body;
+
+    const updatableFields = {
+        title, description,
+        comment,
+        raisedFor,
+    };
+    try {
+        await Query.findByIdAndUpdate(queryId, updatableFields);
+    } catch (err) {
+        const error = new RequestError("Error fetching query", 500, err);
+        return next(error);
+    }
+    await res
+        .status(201)
+        .json({"status": "success", "updatedFields": updatableFields});
+}
+
+const deleteQuery = async (req, res, next) => {
+    const raisedBy = req.userData.userId;
+    const queryId = req.params.queryId;
+    let query;
+    try {
+        query = await Query.findById(queryId);
+    } catch (err) {
+        const error = new RequestError('Fetching query failed, please try again later.', 500, err);
+        return next(error);
+    }
+    if (!query) {
+        const error = new RequestError('query not found', 404);
+        return next(error);
+    }
+    try {
+        if (raisedBy == query.raisedBy) {
+            await Query.deleteOne({_id: queryId});
+        } else {
+            // Add exception for Admins, currectly Query is deleted only by its creator.
+            return res.json({"status": "failed", "message": "invaild queryId for CD"});
+        }
+    } catch (err) {
+        const error = new RequestError('Deleting query failed, please try again later.', 500, err);
+        return next(error);
+    }
+    res.json({"status": "Success", "message": "Query deleted"});
+}
+
+const deleteAllQueries = async (req, res, next) => {
+    try {
+        await Query.deleteMany({});
+    } catch (err) {
+        const error = new RequestError('Deleting queries failed, please try again later.', 500, err);
+        return next(error);
+    }
+    res.json({"status": "Success", "message": "All queries deleted"});
 }
 
 
 exports.getQueryyId = getQueryyId;
 exports.getAllQuery = getAllQuery;
+exports.createQuery = createQuery;
+exports.updateQuery = updateQuery;
+exports.deleteQuery = deleteQuery;
+exports.deleteAllQueries = deleteAllQueries;
