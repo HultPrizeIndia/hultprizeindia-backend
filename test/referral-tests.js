6b86c877e707d1d379adbd179b836d705a63689f
dramikei <raghav2011rv@gmail.com>
2020-10-12 02:07:13 +0530

    Fixed Referral test header problem

--- test/referral-tests.js
+++ test/referral-tests.js
@@ -24,57 +24,58 @@ describe("Referral Routes", () => {
         });
     });
     
-    // won't pass as auth token, not provided
+    describe("Test POST Routes", () => {
+        it("Create referral with correct input", (done) => {
+            request(server)
+                .post("/api/v1/referral/create")
+                .set('Authorization', `Bearer ${process.env.TEST_CD_TOKEN}`)
+                .send({
+                    "description": "It a test referral",
+                    "name": "Test_Verma",
+                    "email": "shivam@test.com",
+                    "mobile": "9953798220"
+                })
+                .end((err, res) => {
+                    if (err) done(err);
+                    expect(res.status).to.equal(200);
+                    expect(res.body.status).to.equal("success");
+                    expect(res.body.referral).to.not.be.undefined;
+                    done();
+                });
+        });
     
-    // describe("Test POST Routes", () => {
-    //     it("Create referral with correct input", (done) => {
-    //         request(server)
-    //             .post("/api/v1/referral/create")
-    //             .send({
-    //                 "description": "It a test referral",
-    //                 "name": "Test_Verma",
-    //                 "email": "shivam@test.com",
-    //                 "mobile": "9953798220"
-    //             })
-    //             .end((err, res) => {
-    //                 if (err) done(err);
-    //                 expect(res.status).to.equal(200);
-    //                 expect(res.body.status).to.equal("success");
-    //                 expect(res.body.referral).to.not.be.undefined;
-    //                 done();
-    //             });
-    //     });
-    //
-    //     it("Create referral with incorrect input", (done) => {
-    //         request(server)
-    //             .post("/api/v1/referral/create")
-    //             .send({
-    //                 "description": "It a test referral",
-    //                 // "name": "Test_Verma",
-    //                 // "email": "shivam@test.com",
-    //                 "mobile": "9953798220"
-    //             })
-    //             .end((err, res) => {
-    //                 if (err) done(err);
-    //                 expect(res.status).to.equal(422);
-    //                 expect(res.body.status).to.equal("failed");
-    //                 done();
-    //             });
-    //     });
-    // });
+        it("Create referral with incorrect input", (done) => {
+            request(server)
+                .post("/api/v1/referral/create")
+                .set('Authorization', `Bearer ${process.env.TEST_CD_TOKEN}`)
+                .send({
+                    "description": "It a test referral",
+                    // "name": "Test_Verma",
+                    // "email": "shivam@test.com",
+                    "mobile": "9953798220"
+                })
+                .end((err, res) => {
+                    if (err) done(err);
+                    expect(res.status).to.equal(422);
+                    expect(res.body.status).to.equal("failed");
+                    done();
+                });
+        });
+    });
     
-    // describe("Test DELETE Routes", () => {
-    //     it("it should delete all referrals", (done) => {
-    //         request(server)
-    //             .delete("/api/v1/referral/delete/all")
-    //             .end((err, res) => {
-    //                 if (err) done(err);
-    //                 expect(res.status).to.equal(200);
-    //                 expect(res.body.status).to.equal("success");
-    //                 done();
-    //             });
-    //     });
-    // });
+    describe("Test DELETE Routes", () => {
+        it("it should delete all referrals", (done) => {
+            request(server)
+                .delete("/api/v1/referral/deleteAll")
+                .set('Authorization', `Bearer ${process.env.TEST_ADMIN_TOKEN}`)
+                .end((err, res) => {
+                    if (err) done(err);
+                    expect(res.status).to.equal(200);
+                    expect(res.body.status).to.equal("success");
+                    done();
+                });
+        });
+    });
 
 
 });
